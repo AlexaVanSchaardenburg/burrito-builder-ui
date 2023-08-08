948ebc31bbc21fa36e4a876ffbca551bebef2de8
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-08-08 10:37:01 -0600

    feat: Complete testing user pathways

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -46,8 +46,18 @@ describe("Burrito Builder", () => {
     cy.get('h1').should('have.text', 'Burrito Builder')
     cy.get('form').should('be.visible')
     cy.get('section').children().should('have.length', 3)
+
+    cy.get('section').children().first().within(($section) => {
+      cy.get('h3').should('have.text', 'Pat')
+      cy.get('.ingredient-list').children().should('have.length', 5).first().should('have.text', 'beans')
+    })
+
+    cy.get('section').children().last().within(($section) => {
+      cy.get('h3').should('have.text', 'Alex')
+      cy.get('.ingredient-list').children().should('have.length', 5).first().should('have.text', 'sofritas')
+    })
   });
-  it.only("should allow users to add orders", () => {
+  it("should allow users to add orders", () => {
     cy.intercept('GET', 'http://localhost:3001/api/v1/orders', {
       statusCode: 200,
       body: {
@@ -89,22 +99,18 @@ describe("Burrito Builder", () => {
           ]
       }
     })
-    // cy.intercept('POST', 'http://localhost:3001/api/v1/orders', {
-    //   statusCode: 200,
-    //   body: {
-    //     "orders": [
-    //       {
-    //       "id": 4,
-    //       "name": "Pepper",
-    //       "ingredients": [
-    //       "beans",
-    //       "hot sauce",
-    //       "carnitas"
-    //       ]
-    //       }
-    //       ]
-    //   }
-    // }).as('addOrder')
+    cy.intercept('POST', 'http://localhost:3001/api/v1/orders', {
+      statusCode: 200,
+      body: {
+          "id": 4,
+          "name": "Pepper",
+          "ingredients": [
+          "beans",
+          "hot sauce",
+          "carnitas"
+          ]
+          }
+    }).as('addOrder')
     cy.visit("http://localhost:3000/");
 
     cy.get('input').type('Pepper')
@@ -114,13 +120,73 @@ describe("Burrito Builder", () => {
 
     cy.get(':nth-child(16)').click()
 
-    // cy.wait('@addOrder')
+    cy.wait('@addOrder')
 
     cy.get('section').children().should('have.length', 4)
     // cy.get('section').children().first().should('have.length', 1)
+
+    //add check that the last card now contains the expected info
     
   });
-  it("should not allow users to add orders that are missing a name or ingredients", () => {
-    cy.visit("http://localhost:3000/");
+it("should not allow users to add orders that are missing a name or ingredients", () => {
+    cy.intercept('GET', 'http://localhost:3001/api/v1/orders', {
+      statusCode: 200,
+      body: {
+        "orders": [
+          {
+          "id": 1,
+          "name": "Pat",
+          "ingredients": [
+          "beans",
+          "lettuce",
+          "carnitas",
+          "queso fresco",
+          "jalapeno"
+          ]
+          },
+          {
+          "id": 2,
+          "name": "Sam",
+          "ingredients": [
+          "steak",
+          "pico de gallo",
+          "lettuce",
+          "carnitas",
+          "queso fresco",
+          "jalapeno"
+          ]
+          },
+          {
+          "id": 3,
+          "name": "Alex",
+          "ingredients": [
+          "sofritas",
+          "beans",
+          "sour cream",
+          "carnitas",
+          "queso fresco"
+          ]
+          }
+          ]
+      }
+    });
+
+    cy.get(':nth-child(16)').click()
+
+    cy.get(':nth-child(15)').should('be.visible').should('have.text', 'Please complete your order')
+
+    cy.get('input').type('Pepper')
+
+    cy.get(':nth-child(16)').click()
+
+    cy.get(':nth-child(15)').should('be.visible').should('have.text', 'Please complete your order')
+
+    cy.get('[name="beans"]').click()
+    cy.get('[name="hot sauce"]').click()
+    cy.get('[name="carnitas"]').click()
+
+    cy.get(':nth-child(16)').click()
+
+    cy.get(':nth-child(15)').should('be.visible').should('have.text', 'Please complete your order')
   });
 });
